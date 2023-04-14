646f1f438ee44baa2a30f4312a45aa70ec0bfc30
unknown <ahmedirheem2003@gmail.com>
2023-04-15 00:47:32 +0300

    Data Collection

---
+++ public/data/sidebar.js
@@ -0,0 +1,93 @@
+const leftSideBarMenu = [
+  {
+    icon: <i class="fa-regular fa-gamepad-modern"></i>,
+    title: 'Gaming',
+    menu: ['Valheim', 'Genshin Impact', 'Minecraft', 'Pokimane', 'Halo Infinite', 'Call of Duty: Warzone', 'Path of Exile', 'Hollow Knight: Silksong', 'Escape from Tarkov', 'Watch Dogs: Legion'],
+  },
+  {
+    icon: <i class="fa-regular fa-tennis-ball"></i>,
+    title: 'Sports',
+    menu: ['NFL', 'NBL', 'Megan Anderson', 'Atlanta Hawks', 'Los Angeles Lakers', 'Boston Celtics', 'Arsenal F.C.', 'Philadelphia 76ers', 'Premier League', 'UFC'],
+  },
+  {
+    icon: <i class="fa-solid fa-business-time"></i>,
+    title: 'Business, Economics, and Finance',
+    menu: ['GameStop', 'Moderna', 'Pfizer', 'Johnson & Johnson', 'AstraZeneca', 'Walgreens', 'Best Buy', 'Novavax', 'SpaceX', 'Tesla'],
+  },
+  {
+    icon: <i class="fa-light fa-hexagon-image"></i>,
+    title: 'Crypto',
+    menu: ['Cardano', 'Dogecoin', 'Algorand', 'Bitcoin', 'Litecoin', 'Basic Attention Token', 'Bitcoin Cash'],
+  },
+  {
+    icon: <i class="fa-sharp fa-light fa-tv-retro"></i>,
+    title: 'Television',
+    menu: ['The Real Housewives of Atlanta', 'The Bachelor', 'Sister Wives', '90 Day Fiance', 'Wife Swap', 'The Amazing Race Australia', 'Married at First Sight', 'The Real Housewives of Dallas', 'My 600-lb Life', 'Last Week Tonight with John Oliver'],
+  },
+  {
+    icon: <i class="fa-regular fa-star"></i>,
+    title: 'Celebrity',
+    menu: ['Kim Kardashian', 'Doja Cat', 'Iggy Azalea', 'Anya Taylor-Joy', 'Jamie Lee Curtis', 'Natalie Portman', 'Henry Cavill', 'Millie Bobby Brown', 'Tom Hiddleston', 'Keanu Reeves'],
+  },
+  {
+    icon: <i class="fa-solid fa-ellipsis"></i>,
+    title: 'More Topics',
+    menu: ['Animals and Pets', 'Anime', 'Art', 'Cars and Motor Vehicles', 'Crafts and DIY', 'Culture, Race, and Ethnicity', 'Ethics and Philosophy', 'Fashion', 'Food and Drink', 'History', 'Hobbies', 'Law', 'Learning and Education', 'Military', 'Movies', 'Music', 'Place', 'Podcasts and Streamers', 'Politics', 'Programming', 'Reading, Writing, and Literature', 'Religion and Spirituality', 'Science', 'Tabletop Games', 'Technology', 'Travel'],
+  },
+]
+
+const settingMenu = [
+  {
+    icon: <i class="fa-regular fa-circle-question"></i>,
+    title: 'Help Center',
+  },
+  {
+    icon: <i class="fa-regular fa-circle-info"></i>,
+    title: 'More',
+    menu: ['Reddit iOS', 'Reddit Android', 'Rereddit', 'Best Communities', 'Communities', 'About Reddit', 'Blog', 'Careers', 'Press'],
+  },
+  {
+    icon: <i class="fa-light fa-clipboard-list-check"></i>,
+    title: 'Terms & Policies',
+    menu: ['User Agreement', 'Privacy Policy', 'Content Policy', 'Moderator Code of Conduct'],
+  },
+  {
+    icon: <i class="fa-regular fa-volume"></i>,
+    title: 'Advertise on Reddit',
+  },
+  {
+    icon: <i class="fa-sharp fa-regular fa-arrow-right-to-bracket"></i>,
+    title: 'Log In / Sign Up',
+  },
+]
+
+const rightSideMenu = [
+  {
+    title: 'POPULAR COMMUNITIES',
+    menu: ['AskReddit', 'NoStupidQuestions','DestinyTheGame', 'explainlikeimfive', 'AskMen', 'leagueoflegends', 'Minecraft']
+  },
+  {
+    title: 'GAMING',
+    menu: ['StardewValley', 'FortniteCompetitive', 'Warframe', 'totalwar', 'Fallout', 'RocketLeague', 'fo76', 'yugioh', 'eu4'],
+  },
+  {
+    title: 'SPORTS',
+    menu: ['running', 'soccer', 'bjj', 'MMA', 'hockey', 'formula1', 'CFB', 'barstoolsports', 'airsoft', 'rugbyunion', 'golf', 'MTB', 'cycling'],
+  },
+  {
+    title: 'TV',
+    menu: ['Naruto', 'BokuNoHeroAcademia', 'marvelstudios', 'rupaulsdragrace', '90DayFiance', 'dbz', 'Boruto']
+  },
+  {
+    title: 'TRAVEL',
+    menu: ['vancouver', 'brasil', 'australia', 'mexico' , 'argentina', 'melbourne', 'ottawa', 'korea', 'ireland', 'travel', 'Calgary', 'portugal']
+  },
+  {
+    title: 'HEALTH & FITNESS',
+    menu: ['orangetheory', 'bodybuilding', 'bodyweightfitness', 'vegan', 'crossfit', 'nattyorjuice', 'EatCheapAndHealthy', 'EatCheapAndHealthy', 'loseit']
+  },
+  {
+    title: 'FASHION',
+    menu: ['MakeupAddiction', 'Watches', 'BeautyGuruChatter', 'femalefashionadvice', 'frugalmalefashion', 'curlyhair', 'poshmark']
+  },
+]
