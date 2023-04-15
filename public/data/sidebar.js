d17b1b05e11512d70695f9f593af391c65823e15
unknown <ahmedirheem2003@gmail.com>
2023-04-15 07:53:05 +0300

    Header Section

--- public/data/sidebar.js
+++ public/data/sidebar.js
@@ -1,67 +1,69 @@
-const leftSideBarMenu = [
+const leftSidebarMenuData = [
   {
-    icon: <i class="fa-regular fa-gamepad-modern"></i>,
+    icon: 'fa-solid fa-gamepad',
     title: 'Gaming',
     menu: ['Valheim', 'Genshin Impact', 'Minecraft', 'Pokimane', 'Halo Infinite', 'Call of Duty: Warzone', 'Path of Exile', 'Hollow Knight: Silksong', 'Escape from Tarkov', 'Watch Dogs: Legion'],
   },
   {
-    icon: <i class="fa-regular fa-tennis-ball"></i>,
+    icon: 'fa-solid fa-baseball',
     title: 'Sports',
     menu: ['NFL', 'NBL', 'Megan Anderson', 'Atlanta Hawks', 'Los Angeles Lakers', 'Boston Celtics', 'Arsenal F.C.', 'Philadelphia 76ers', 'Premier League', 'UFC'],
   },
   {
-    icon: <i class="fa-solid fa-business-time"></i>,
+    icon: 'fa-solid fa-business-time',
     title: 'Business, Economics, and Finance',
     menu: ['GameStop', 'Moderna', 'Pfizer', 'Johnson & Johnson', 'AstraZeneca', 'Walgreens', 'Best Buy', 'Novavax', 'SpaceX', 'Tesla'],
   },
   {
-    icon: <i class="fa-light fa-hexagon-image"></i>,
+    icon: 'fa-regular fa-snowflake',
     title: 'Crypto',
     menu: ['Cardano', 'Dogecoin', 'Algorand', 'Bitcoin', 'Litecoin', 'Basic Attention Token', 'Bitcoin Cash'],
   },
   {
-    icon: <i class="fa-sharp fa-light fa-tv-retro"></i>,
+    icon: 'fa-solid fa-tv',
     title: 'Television',
     menu: ['The Real Housewives of Atlanta', 'The Bachelor', 'Sister Wives', '90 Day Fiance', 'Wife Swap', 'The Amazing Race Australia', 'Married at First Sight', 'The Real Housewives of Dallas', 'My 600-lb Life', 'Last Week Tonight with John Oliver'],
   },
   {
-    icon: <i class="fa-regular fa-star"></i>,
+    icon: 'fa-regular fa-star',
     title: 'Celebrity',
     menu: ['Kim Kardashian', 'Doja Cat', 'Iggy Azalea', 'Anya Taylor-Joy', 'Jamie Lee Curtis', 'Natalie Portman', 'Henry Cavill', 'Millie Bobby Brown', 'Tom Hiddleston', 'Keanu Reeves'],
   },
   {
-    icon: <i class="fa-solid fa-ellipsis"></i>,
+    icon: 'fa-solid fa-ellipsis',
     title: 'More Topics',
     menu: ['Animals and Pets', 'Anime', 'Art', 'Cars and Motor Vehicles', 'Crafts and DIY', 'Culture, Race, and Ethnicity', 'Ethics and Philosophy', 'Fashion', 'Food and Drink', 'History', 'Hobbies', 'Law', 'Learning and Education', 'Military', 'Movies', 'Music', 'Place', 'Podcasts and Streamers', 'Politics', 'Programming', 'Reading, Writing, and Literature', 'Religion and Spirituality', 'Science', 'Tabletop Games', 'Technology', 'Travel'],
   },
 ]
 
-const settingMenu = [
+const settingMenuData = [
   {
-    icon: <i class="fa-regular fa-circle-question"></i>,
+    icon: 'fa-regular fa-circle-question',
     title: 'Help Center',
   },
   {
-    icon: <i class="fa-regular fa-circle-info"></i>,
+    icon: 'fa-solid fa-circle-info',
     title: 'More',
+    moreIcon: 'fa-solid fa-angle-down',
     menu: ['Reddit iOS', 'Reddit Android', 'Rereddit', 'Best Communities', 'Communities', 'About Reddit', 'Blog', 'Careers', 'Press'],
   },
   {
-    icon: <i class="fa-light fa-clipboard-list-check"></i>,
+    icon: 'fa-regular fa-rectangle-list',
     title: 'Terms & Policies',
+    moreIcon: 'fa-solid fa-angle-down',
     menu: ['User Agreement', 'Privacy Policy', 'Content Policy', 'Moderator Code of Conduct'],
   },
   {
-    icon: <i class="fa-regular fa-volume"></i>,
+    icon: 'fa-solid fa-volume-high',
     title: 'Advertise on Reddit',
   },
   {
-    icon: <i class="fa-sharp fa-regular fa-arrow-right-to-bracket"></i>,
+    icon: 'fa-solid fa-arrow-right-to-bracket',
     title: 'Log In / Sign Up',
   },
 ]
 
-const rightSideMenu = [
+const rightSideMenuData = [
   {
     title: 'POPULAR COMMUNITIES',
     menu: ['AskReddit', 'NoStupidQuestions','DestinyTheGame', 'explainlikeimfive', 'AskMen', 'leagueoflegends', 'Minecraft']
