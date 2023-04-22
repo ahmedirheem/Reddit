cbd2d6a224cb3422ce953a4cceaf8677c011f4de
unknown <ahmedirheem2003@gmail.com>
2023-04-22 21:28:22 +0300

    Create Post Page Header

---
+++ public/create-post/script.js
@@ -0,0 +1,61 @@
+const settingBtn = document.querySelector('.setting-btn');
+const settingMenu = document.querySelector('.setting-menu');
+
+const createHtmlElement = (element, className, id, textContent) => {
+  const ele = document.createElement(element);
+
+  if (className) {
+    ele.className = className;
+  }
+  if (id) {
+    ele.id = id;
+  }
+  if (textContent) {
+    ele.textContent = textContent;
+  }
+
+  return ele;
+};
+
+const appendChildren = (parent, ...children) => {
+  children.forEach((child) => {
+    parent.appendChild(child);
+  });
+};
+
+// eslint-disable-next-line no-unused-vars
+const appearSettingMenu = (ele, i) => {
+  ele.nextSibling.classList.toggle('active');
+  ele.children[i].classList.toggle('rotated');
+};
+
+settingBtn.addEventListener('click', () => {
+  settingMenu.classList.toggle('active');
+});
+
+// eslint-disable-next-line no-undef
+settingMenuData.forEach((item) => {
+  const menuItem = createHtmlElement('li', 'setting-item');
+  menuItem.setAttribute('onclick', 'appearSettingMenu(this, 2)');
+  const itemTitle = createHtmlElement('span', 'item-title', null, item.title);
+  const itemIcon = createHtmlElement('i', item.icon);
+  const itemMoreIcon = createHtmlElement('i', item.moreIcon);
+
+  if (item.menu) {
+    const subMenu = createHtmlElement('ul', 'sub-setting-menu');
+
+    item.menu.forEach((subItem) => {
+      const subMenuItemLink = createHtmlElement('a', 'submenu-item-link');
+      subMenuItemLink.setAttribute('href', '#');
+      const subMenuItemList = createHtmlElement('li', 'submenu-item-list', null, subItem);
+      subMenuItemLink.appendChild(subMenuItemList);
+      subMenu.appendChild(subMenuItemLink);
+    });
+
+    appendChildren(menuItem, itemIcon, itemTitle, itemMoreIcon);
+    appendChildren(settingMenu, menuItem, subMenu);
+  } else {
+    appendChildren(menuItem, itemIcon, itemTitle, itemMoreIcon);
+    settingMenu.appendChild(menuItem);
+  }
+});
