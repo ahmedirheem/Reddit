1aa71ba09dbdab20fd0ea5dc469e2c83f4af1d85
unknown <ahmedirheem2003@gmail.com>
2023-04-22 13:17:27 +0300

    CheckAuthentication Middlewar

---
+++ server/middlewares/checkAuth.js
@@ -0,0 +1,18 @@
+const { verifyToken, CustomError } = require('../utils');
+
+const checkAuth = (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    throw new CustomError('Unauthorized!', 400);
+  }
+
+  verifyToken({ token })
+    .then((decoded) => {
+      req.user = decoded;
+      next();
+    })
+    .catch((err) => next(err));
+};
+
+module.exports = checkAuth;
