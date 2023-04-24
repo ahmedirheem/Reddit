a4f0b1adf72f30f04060deb920588ee210a96991
unknown <ahmedirheem2003@gmail.com>
2023-04-25 00:24:04 +0300

    Fetch User Posts

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,17 @@
 const userPostsContainer = document.querySelector('.user-posts-container');
 
-window.onload
+window.onload = () => {
+  fetch('/api/v1/post/user-posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ posterId: 2 }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      // eslint-disable-next-line no-undef
+      data.data.posts.forEach((post) => createPostElement(post));
+    })
+    .catch((err) => console.log(err));
+};
